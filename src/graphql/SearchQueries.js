c1c2dd5b7dcbe3f6b249e7724d76a5125e5e5206
Pete Taylor <pete.jc.taylor@hey.com>
2021-01-27 09:02:34 -0500

    not ordering search at the frontend query now, letting the ts_rank function do that in pg

--- src/graphql/SearchQueries.js
+++ src/graphql/SearchQueries.js
@@ -2,12 +2,7 @@ import { gql } from '@apollo/client'
 
 export const FULL_TEXT_SEARCH = gql`
     query Search($search: String!, $limit: Int!, $offset: Int!) {
-        full_search(
-            args: { search: $search }
-            order_by: { name: asc }
-            limit: $limit
-            offset: $offset
-        ) {
+        full_search(args: { search: $search }, limit: $limit, offset: $offset) {
             name
             common_english
             searchable_id
